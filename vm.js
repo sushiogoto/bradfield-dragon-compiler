103382ea95afe21f0d6977f94aa58b38fad7d8c1
Yoshio Goto <yoshiogoto@YoshioGoto-MBP2.local>
2017-05-05 20:43:56 -0700

    WIP

--- vm.js
+++ vm.js
@@ -4,33 +4,26 @@ const fs = require('fs')
 // 0x10 'push' pushes an immediate value (i.e. an allowed value not a pointer) onto the operand stack
 // 0x11 'add'  adds the top two numbers on the stack and pushes the result back onto stack
 // 0x12 'sub'  subtracts the top two numbers on the stack and pushes the result back onto stack
+// 0x13 'gstore' stores value at top of stack in global array, at index arg
+// 0x14 'gload'  loads from global array (at index arg) onto stack
+// 0x15 'jump'   mutate instruction pointer to value given by arg
+// 0x16 'jump_if_false'  `jump` but only if value at top of stack is false
+// 0x17 'inc' increment value at top of stack by one
+// 0x18 'less_than'  remove top two stack items--push true to stack if first-popped less than second-popped, else push false
 // 0xFF 'halt' stops the interpreter
 
-// var code = [
-// 	['push', 3],
-// 	['push', 4],
-// 	['add'],
-// 	['push', 1],
-// 	['sub'],
-// 	['halt']
-// ];
 
-// var code = [
-// 	[0x10, 3],
-// 	[0x10, 4],
-// 	[0x11],
-// 	[0x10, 1],
-// 	[0x12],
-// 	[0xFF]
-// ];
+function run (code, meta) {
+  const globalSize = meta.readUInt16BE()
+  let globals = new Array(globalSize)
 
-function run (code) {
 	var operands = []
 	var ip = 0 // aka entryPoint, doesn't have to start at 0
 
 	while (ip < code.length) {
 		var currentInstruction = code[ip]
     let a, b, result
+    console.log({currentInstruction, ip, operands, globals})
     ip++
 
 		switch (currentInstruction) {
@@ -54,17 +47,54 @@ function run (code) {
 				result = a - b
 				operands.push(result)
 				break
+      // gstore
+      case 0x13:
+        a = operands.pop()
+        b = code.slice(ip, ip += 2).readUInt16BE()
+        globals[b] = a
+        break
+       // gload
+      case 0x14: 
+        a = code.slice(ip, ip += 2).readUInt16BE()
+        operands.push(globals[a])
+        break
+      // jump
+      case 0x15:
+        ip = code.slice(ip, ip + 2).readUInt16BE()
+        break
+      // jump_if_false
+      case 0x16:
+        a = operands.pop()
+        b = code.slice(ip, ip += 2).readUInt16BE()
+        if (a === false) {
+          ip = b
+        }
+        break
+      // inc
+      case 0x17:
+        a = operands.pop()
+        operands.push(a + 1)
+        break
+      // less_than
+      case 0x18:
+        a = operands.pop()
+        b = operands.pop()
+        operands.push(a < b)
+        break
 			// halt
 			case 0xFF:
-				return operands
-			default:
-				throw new Error('invalid operation')
-		}
-	}
+        console.log(operands)
+        console.log(globals)
+        return
+      default:
+        throw new Error('invalid operation')
+    }
+  }
 }
 
 if (module === require.main) {
-	let code = fs.readFileSync('./examples/bytecode-programs/program-a/code')
+  const code = fs.readFileSync('./examples/bytecode-programs/program-b/code')
+	const meta = fs.readFileSync('./examples/bytecode-programs/program-b/meta')
 
-  console.log(run(code)) // [-6]
+  run(code, meta) // [-6]
 }
