219d9d292de640461c0cae346e307fa2b6a3fc10
Yoshio Goto <yoshiogoto@YoshioGoto-MBP2.local>
2017-05-05 18:51:41 -0700

    Adds bytecode values

--- vm.js
+++ vm.js
@@ -1,10 +1,25 @@
+// first 16 codes are reserved in case we need them for special codes later on
+// 0x10 'push' pushes an immediate value (i.e. an allowed value not a pointer) onto the operand stack
+// 0x11 'add'  adds the top two numbers on the stack and pushes the result back onto stack
+// 0x12 'sub'  subtracts the top two numbers on the stack and pushes the result back onto stack
+// 0xFF 'halt' stops the interpreter
+
+// var code = [
+// 	['push', 3],
+// 	['push', 4],
+// 	['add'],
+// 	['push', 1],
+// 	['sub'],
+// 	['halt']
+// ];
+
 var code = [
-	['push', 3],
-	['push', 4],
-	['add'],
-	['push', 1],
-	['sub'],
-	['halt']
+	[0x10, 3],
+	[0x10, 4],
+	[0x11],
+	[0x10, 1],
+	[0x12],
+	[0xFF]
 ];
 
 function run (code) {
@@ -16,22 +31,26 @@ function run (code) {
 		var currentArg = code[ip][1];
 
 		switch (currentInstruction) {
-			case 'push':
+			// push
+			case 0x10:
 				operands.push(currentArg);
 				break
-			case 'add':
+			// add
+			case 0x11:
 				var a = operands.pop();
 				var b = operands.pop();
 				var result = a + b;
 				operands.push(result);
 				break
-			case 'sub':
+			// sub
+			case 0x12:
 				var a = operands.pop();
 				var b = operands.pop();
 				var result = a - b;
 				operands.push(result);
 				break
-			case 'halt':
+			// halt
+			case 0xFF:
 				return operands;
 			default:
 				throw new Error('invalid operation');
