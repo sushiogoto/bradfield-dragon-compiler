d84ea34b1e73a0a683649634dc4558e9a994617a
Yoshio Goto <yoshiogoto@YoshioGoto-MBP2.local>
2017-05-05 19:40:46 -0700

    WIP

--- vm.js
+++ vm.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 // first 16 codes are reserved in case we need them for special codes later on
 // 0x10 'push' pushes an immediate value (i.e. an allowed value not a pointer) onto the operand stack
 // 0x11 'add'  adds the top two numbers on the stack and pushes the result back onto stack
@@ -13,51 +15,56 @@
 // 	['halt']
 // ];
 
-var code = [
-	[0x10, 3],
-	[0x10, 4],
-	[0x11],
-	[0x10, 1],
-	[0x12],
-	[0xFF]
-];
+// var code = [
+// 	[0x10, 3],
+// 	[0x10, 4],
+// 	[0x11],
+// 	[0x10, 1],
+// 	[0x12],
+// 	[0xFF]
+// ];
 
 function run (code) {
-	var operands = [];
-	var ip = 0; // aka entryPoint, doesn't have to start at 0
+	var operands = []
+	var ip = 0 // aka entryPoint, doesn't have to start at 0
 
 	while (ip < code.length) {
-		var currentInstruction = code[ip][0];
-		var currentArg = code[ip][1];
+		var currentInstruction = code[ip]
+    let a, b, result
+    ip++
 
 		switch (currentInstruction) {
 			// push
 			case 0x10:
-				operands.push(currentArg);
+        a = code.slice(ip, ip + 2).readInt16BE()
+        ip += 2
+				operands.push(a)
 				break
 			// add
 			case 0x11:
-				var a = operands.pop();
-				var b = operands.pop();
-				var result = a + b;
-				operands.push(result);
+				a = operands.pop()
+				b = operands.pop()
+				result = a + b
+				operands.push(result)
 				break
 			// sub
 			case 0x12:
-				var a = operands.pop();
-				var b = operands.pop();
-				var result = a - b;
-				operands.push(result);
+				a = operands.pop()
+				b = operands.pop()
+				result = a - b
+				operands.push(result)
 				break
 			// halt
 			case 0xFF:
-				return operands;
+				return operands
 			default:
-				throw new Error('invalid operation');
+				throw new Error('invalid operation')
 		}
-
-		ip++;
 	}
 }
 
-console.log(run(code)); // [-6]
+if (module === require.main) {
+	let code = fs.readFileSync('./examples/bytecode-programs/program-a/code')
+
+  console.log(run(code)) // [-6]
+}
